d74174ebd53cd4a1cb8dd9ad323e9f52c43948ca
sarah-grade <skgrade6@gmail.com>
2019-10-08 15:05:33 -0700

    added comments to query.py

--- MyApp/query.py
+++ MyApp/query.py
@@ -10,15 +10,20 @@ import pandas as pd
 data = pd.read_csv('species.csv', encoding='utf-8')
 
 def findStatus(commonName, scientificName):
-    # search for row with common name
+    # search for row with common name/scientific name depending on what they gave us
     # extract status
     if commonName != "":
         if commonName not in data["Common Name"].values:
+            # if not in database tell them
             return "The " + commonName + " is not in our database."
+        # otherwise extract the status associated with that name
         return "The " + commonName + " is classified as " + data[data["Common Name"] == commonName]["Status"].values[0]
     elif scientificName != "":
         if scientificName not in data["Scientific Name"].values:
+                # if not in database tell them
                 return "The " + scientificName + " is not in our database."
+        # otherwise extract the status associated with that name
         return "The " + scientificName + " is classified as " + data[data["Scientific Name"] == scientificName]["Status"].values[0]
     else:
+        # no name was submitted
         return "Please enter a name!!"
